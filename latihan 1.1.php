f1ac25061d67b25864e203760deed2dd7c5e612a
Octavia864 <octahafizhah05@gmail.com>
2024-08-19 08:50:19 +0700

    Pertemuan 1PBO

---
+++ latihan 1.1.php
@@ -0,0 +1,8 @@
+Class Mobil { // ini adalah contoh class
+    // ini adalah body class
+}
+
+$ObjekBMW = new Mobil ; // ini adalah objek BMW dari class mobil 
+$ObjekTesla = new Mobil ;// ini adalah obkek Tesla dari class mobil 
+$ObjekAudi = new Mobil ;// ini adalah objek Audi dari class mobil
+
