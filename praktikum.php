fa6edd0da2538dea19b1a3739c4f80b040af52cb
Octavia864 <octahafizhah05@gmail.com>
2024-08-26 12:32:49 +0700

    pertemuan3

---
+++ praktikum.php
@@ -0,0 +1,31 @@
+<?php
+class PinjamanSyariah
+{
+    var $pinjaman = 1000000;
+    var $besarbunga = 10;
+    var $Lama_angsuran = 5;
+}
+// Fungsi untuk menghitung angsuran bulanan
+function this -> hitungAngsuran($pinjaman = 1000000, $besarbunga = 10 , $Lama_angsuran = 5 ) 
+// Fungsi Menghitung total pinjaman ] per bulan
+function this-> hitungJumlahAgsuran $pinjaman=($Lama_angsuran = 5 besarbunga /10)
+    
+    // Menghitung jumlah total angsuran bulanan
+    $totalAngsuran = this->   ($pinjaman * $besarbunga) + ($Lama_angsuran * 5)));
+    
+    return $Besaran_Angsuran;
+{
+ 
+// Input data
+$pinjaman = 10000000; // Jumlah pinjaman (misalnya 10 juta)
+$besarbunga = 10; // Bunga per bulan (misalnya 10%)
+$Lama_angsuran = 5; // Jangka waktu pinjaman dalam tahun (misalnya 5 Bulan)
+ 
+// Menghitung angsuran bulanan
+$Besaran_angsuran = hitungAngsuran($pinjaman, $besarbunga, $Lama_angsuran);
+ 
+// Menampilkan hasil
+echo "Angsuran bulanan: "($Besaran_angsuran, 5, ',', '.') ;
+Jl Keadilan No 16
+Telp.7235349
+}
