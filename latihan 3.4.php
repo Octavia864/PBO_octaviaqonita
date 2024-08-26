9f2c7b2efe81cde14f0665ff157dafca4ce6b132
Octavia864 <octahafizhah05@gmail.com>
2024-08-26 12:32:24 +0700

    pertemuan3

---
+++ latihan 3.4.php
@@ -0,0 +1,28 @@
+<?php
+class angsuran
+{
+    var $pinjaman = 1000000;
+    var $besarbunga = 10;
+    var $Total_pinjaman = 1100000;
+    var $Lama_angsuran = 5;
+    var $Besaran_angsuran = 2200000
+    function pinjaman
+        if ($this -> $Lama_angsuran > 5) $pinjaman ='Telat membayar angsuran';
+        else {$status = 'Tidak Telat';
+        return $status;
+        }
+    } 
+
+echo TOKO PEGADAIAN SYARIAH
+Jl Keadilan No 16
+Telp."72353459"
+
+Program Perhitungan Besaran Angsuran Hutang
+//Mengambil input dari penguna
+$pinjaman =1000000;
+$bunga = 10;
+$bulan = 5;
+//Menghitung total pinjaman 
+$total_pinjaman = $pinjaman + ($pinjaman*$bunga/100);
+//Menghitung besaran angsuran
+$angsuran = 
