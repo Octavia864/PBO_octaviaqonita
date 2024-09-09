05e86f86331b4f0fc2b4396c56fceb3cad43fdb2
Octavia864 <octahafizhah05@gmail.com>
2024-09-09 11:45:38 +0700

    pertemuan 5

---
+++ Latihan5.5.php
@@ -0,0 +1,28 @@
+<?php
+class Kartu {
+$Kartu = 4;
+switch ($Kartu) {
+    case 1:
+        echo "Pembeli 1 mendapatkan diskon 15000";
+        break;
+    case 2:
+        echo "Pembeli 2 mendapatkan diskon 50000";
+        break;
+    case 3:
+        echo "Pembeli 3 mendapatkan diskon 5000";
+        break;
+    case 4:
+        echo "Pembeli 4 tidak mendapatkan diskon";
+        break;
+    default:
+    echo "Kamu belum memiiki kartu member!"
+
+
+$Kartu1 = new Kartu(Memiliki, 200000, '15000', '185000');
+$Kartu2 = new Kartu(Memiliki, 570000, '50000', '520000');
+$Kartu3 = new Kartu(Tidak Memiliki, 120000, '5000', '115000');
+$Kartu4 = new Kartu(Tidak Memiliki, 90000, '0', '90000');
+}
+}
+
+?>
