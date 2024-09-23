1919e22500f0f2246a759ed4fba309074ff55513
Octavia864 <octahafizhah05@gmail.com>
2024-09-23 12:01:02 +0700

    Pertemuan6

---
+++ Latihan6.2.php
@@ -0,0 +1,16 @@
+<?php
+class Perulangan{
+    public function loop () {
+        for($i =15;Si > 0;$i--)    
+        {
+        echo $i;
+        echo "<br/>";
+        }
+    }
+}
+
+$ObjekPerulangan = new Perulangan();
+echo "Jenis Perulangan Lainya "."<br />";
+//echo $ObjekPerulangan->loop() . "<br />";
+
+?>
